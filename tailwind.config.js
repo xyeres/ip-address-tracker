a2a9ce243d3ac29b578bdfa861bedfa2f5d975ca
Michael Carr <xyeres@me.com>
2022-01-24 17:24:36 -0800

    Add bg image

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'theme-pattern': "url('./img/pattern-bg.png')",
+      }
+    },
   },
   plugins: [],
 }
